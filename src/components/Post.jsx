6fa59b9024d960e135d936fa2bc85ee9a6816c6b
rohit-workspace069 <rohitwork069@gmail.com>
2024-07-28 23:34:34 +0530

    minor changes made

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,28 +2,31 @@ import React from "react";
 import "./comonent-style/Post.css";
 
 function Post(props) {
-  return (<div className="postitem">
-    <div className="Post">
-      <div className="user-detail">
-        <div className="user-image">
-          <img src="resources\rohitprofile.jpg" alt="Userprofile" />
-        </div>
-        <div className="user-name">{props.username} </div>
-      </div>
-      <div className="post-content">{props.content}</div>
-      <div className="post-interaction">
-        <div className="like-comment">
-          <div className="like">
-            <i className="fas fa-thumbs-up" /> {props.likeCount}
+  return (
+    <div className="postitem">
+      <div className="Post">
+        <div className="user-detail">
+          <div className="user-image">
+            <img src="resources\rohitprofile.jpg" alt="Userprofile" />
           </div>
-          <div className="comment">
-            <i className="fas fa-comment" /> {props.commentCount}
+          <div className="user-name">{props.username} </div>
+        </div>
+        <div className="post-content">{props.content}</div>
+        <div className="post-interaction">
+          <div className="like-comment">
+            <div className="like">
+              Like-:
+              <i className="fas fa-thumbs-up" /> {props.likeCount}
+            </div>
+            <div className="comment">
+              {" "}
+              Comments-:
+              <i className="fas fa-comment" /> {props.commentCount}
+            </div>
           </div>
         </div>
-        <div className="show-comments">View all {props.commentCount} comments</div>
       </div>
     </div>
-  </div>
   );
 }
 
